a83f71420ed01f98ce1dee9441bb90ce25fd97da
Frank <f.neumann.berlin@googlemail.com>
2024-04-04 09:03:24 +0200

    mit integriertem menu

--- app/src/main/java/com/example/simplemenu/MainActivity.kt
+++ app/src/main/java/com/example/simplemenu/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.simplemenu
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.example.simplemenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setSupportActionBar(binding.mytoolbar.root)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
     }
 }
