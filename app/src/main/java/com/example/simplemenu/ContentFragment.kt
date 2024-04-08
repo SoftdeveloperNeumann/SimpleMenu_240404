7944bcb27f5a72a83a96423be8d88016df0906d7
Frank <f.neumann.berlin@googlemail.com>
2024-04-08 09:37:07 +0200

    mit Menüerweiterung aus dem Fragment

--- app/src/main/java/com/example/simplemenu/ContentFragment.kt
+++ app/src/main/java/com/example/simplemenu/ContentFragment.kt
@@ -2,19 +2,23 @@ package com.example.simplemenu
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.simplemenu.databinding.FragmentContentBinding
 
 
 class ContentFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var binding: FragmentContentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -22,8 +26,21 @@ class ContentFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_content, container, false)
+        binding = FragmentContentBinding.inflate(layoutInflater,container,false)
+
+
+        return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_fragment, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(activity, "Aus dem Fragment", Toast.LENGTH_SHORT).show()
+
+        return super.onOptionsItemSelected(item)
+    }
 
 }
